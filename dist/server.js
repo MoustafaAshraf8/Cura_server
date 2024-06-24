f37d9a7d82cd80601b8d617fb09bfa4e0396dfc9
Mohamed Salah <mohamed.salah@elmaestrocode.com>
2024-06-24 16:07:50 +0300

    removing sleep from the first try

--- dist/server.js
+++ dist/server.js
@@ -1,14 +1,14 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
+var __awaiter = (this && this.__awaiter) || function(thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function(resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function(resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
         function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
+var __importDefault = (this && this.__importDefault) || function(mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
@@ -29,7 +29,7 @@ server.use(route_1.serverRoute.baseUrl, (req, res, next) => {
 });
 server.use(route_1.patientRoute.baseUrl, PatientRouter_1.PatientRouter);
 server.use(route_1.doctorRoute.baseUrl, DoctorRouter_1.DoctorRouter);
-server.get(route_1.serverRoute.baseUrl, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+server.get(route_1.serverRoute.baseUrl, (req, res, next) => __awaiter(void 0, void 0, void 0, function*() {
     const patient = yield index_1.default.Patient.findAll({
         include: [{ model: index_1.default.EMR, as: "emr" }],
     });
@@ -37,8 +37,11 @@ server.get(route_1.serverRoute.baseUrl, (req, res, next) => __awaiter(void 0, vo
 }));
 server.use(errorHandler_1.errorHandler);
 function connectToDB(tries) {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield sleep(5000);
+    return __awaiter(this, void 0, void 0, function*() {
+        if (tries !== 1) {
+            yield sleep(5000);
+        }
+
         if (tries >= 3) {
             throw Error("Cannot Connect To DB!!!");
         }
@@ -55,10 +58,10 @@ function sleep(ms) {
         setTimeout(resolve, ms);
     });
 }
-(() => __awaiter(void 0, void 0, void 0, function* () {
+(() => __awaiter(void 0, void 0, void 0, function*() {
     try {
         yield connectToDB(1);
-        server.listen(port, () => __awaiter(void 0, void 0, void 0, function* () {
+        server.listen(port, () => __awaiter(void 0, void 0, void 0, function*() {
             console.log(`server listening on port: 8080`);
         }));
     }
--- src/server.ts
+++ src/server.ts
@@ -11,53 +11,56 @@ const server: Application = express();
 
 server.use(express.json());
 server.use(
-  serverRoute.baseUrl,
-  (req: Request, res: Response, next: NextFunction) => {
-    console.log("________________");
-    next();
-  }
+    serverRoute.baseUrl,
+    (req: Request, res: Response, next: NextFunction) => {
+        console.log("________________");
+        next();
+    }
 );
 server.use(patientRoute.baseUrl, PatientRouter);
 server.use(doctorRoute.baseUrl, DoctorRouter);
 
 server.get(
-  serverRoute.baseUrl,
-  async (req: Request, res: Response, next: NextFunction) => {
-    const patient = await db.Patient.findAll({
-      include: [{ model: db.EMR, as: "emr" }],
-    });
-    res.json(patient);
-  }
+    serverRoute.baseUrl,
+    async (req: Request, res: Response, next: NextFunction) => {
+        const patient = await db.Patient.findAll({
+            include: [{ model: db.EMR, as: "emr" }],
+        });
+        res.json(patient);
+    }
 );
 
 server.use(errorHandler);
 
 async function connectToDB(tries: number) {
-  await sleep(5000);
-  if (tries >= 3) {
-    throw Error("Cannot Connect To DB!!!");
-  }
-  try {
-    await db.sequelize.authenticate();
-  } catch (error) {
-    await connectToDB(++tries);
-  }
+    if (tries !== 1) {
+        await sleep(5000);
+    }
+
+    if (tries >= 3) {
+        throw Error("Cannot Connect To DB!!!");
+    }
+    try {
+        await db.sequelize.authenticate();
+    } catch (error) {
+        await connectToDB(++tries);
+    }
 }
 
 function sleep(ms: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
 }
 
 (async () => {
-  try {
-    await connectToDB(1);
-    server.listen(port, async () => {
-      console.log(`server listening on port: 8080`);
-    });
-  } catch (error) {
-    console.error(error);
-    process.exit(0);
-  }
+    try {
+        await connectToDB(1);
+        server.listen(port, async () => {
+            console.log(`server listening on port: 8080`);
+        });
+    } catch (error) {
+        console.error(error);
+        process.exit(0);
+    }
 })();
